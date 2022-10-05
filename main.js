f4b138aa957997d94465417afc45b2abfc3e197b
Anankaaa <hegyaljai83@hotmail.com>
2022-10-05 15:14:02 +0200

    font-family added

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const navbar = document.querySelector('.navbar');
+const navLink = document.querySelectorAll('.nav-link')
 
 window.onscroll = function (event) {
     const scroll = window.pageYOffset;
-    if(scroll!=0){
+    if(scroll>0){
         navbar.style.background = 'rgba(0, 0, 0, 0)';
+        navLink.style.color="gray";
     }
   }
