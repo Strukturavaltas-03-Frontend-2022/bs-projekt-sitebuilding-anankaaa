8a99b348655a92d9ee59516df4babcfdaa774184
Anankaaa <hegyaljai83@hotmail.com>
2022-10-06 09:21:27 +0200

    responsivity changes

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const navbar = document.querySelector('.navbar');
-const navLink = document.querySelectorAll('.nav-link')
+const navLinks = document.querySelectorAll('.nav-link');
+const navbarBrand = document.querySelector('.navbar-brand');
 
 window.onscroll = function (event) {
     const scroll = window.pageYOffset;
-    if(scroll>0){
-        navbar.style.background = 'rgba(0, 0, 0, 0)';
-        navLink.style.color="gray";
+    if(scroll !== 0){
+        navbar.style.background = 'white';
+        navbarBrand.style.color = 'gray';
+        changeNavLinkColor('gray')
+    }else{
+        navbar.style.background = '';
+        navbarBrand.style.color = '';
+        changeNavLinkColor('');
     }
   }
+
+  const changeNavLinkColor = (color) => {
+    navLinks.forEach(link => {
+        link.style.color = color;
+    }) 
+  } 
